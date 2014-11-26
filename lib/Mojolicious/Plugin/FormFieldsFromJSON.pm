1e457bde4d0ef1b65c1d0fe9ef016eaa2b263a89
reneeb <github@renee-baecker.de>
2014-11-26 17:29:03 +0100

    add some code to create the fields

--- lib/Mojolicious/Plugin/FormFieldsFromJSON.pm
+++ lib/Mojolicious/Plugin/FormFieldsFromJSON.pm
@@ -1,20 +1,101 @@
 package Mojolicious::Plugin::FormFieldsFromJSON;
 use Mojo::Base 'Mojolicious::Plugin';
 
+# ABSTRACT: create form fields based on a definition in a JSON file
+
 our $VERSION = '0.01';
 
+use Carp;
+use File::Spec;
+
+use Mojo::Asset::File;
+use Mojo::JSON qw(decode_json);
+
+our %request;
+
 sub register {
-  my ($self, $app) = @_;
+    my ($self, $app, $config) = @_;
+  
+    my %configs;
+    my $dir = $config->{dir} || '.';
+  
+    my %valid_types = (
+        text     => 1,
+        checkbox => 1,
+        select   => 1,
+        radio    => 1,
+    );
+  
+    $app->helper(
+        'form_fields' => sub {
+            my ($c, $file) = @_;
+  
+            return '' if !$file;
+  
+            if ( !$configs{$file} ) {
+                my $path = File::Spec->catfile( $dir, $file . '.json' );
+                return '' if !-r $path;
+  
+                eval {
+                    my $content = Mojo::Asset::File->new( path => $path )->slurp;
+                    $configs{$file} = decode_json $content;
+                } or do {
+                    $app->log->error( "FORMFIELDS $file: $@" );
+                    return '';
+                };
+  
+                if ( 'ARRAY' ne ref $configs{$file} ) {
+                    $app->log->error( 'Definition JSON must be an ARRAY' );
+                    return '';
+                }
+            }
+  
+            return '' if !$configs{$file};
+  
+            my $config = $configs{$file};
+  
+            FIELD:
+            for my $field ( @{ $config } ) {
+                if ( 'HASH' ne ref $field ) {
+                    $app->log->error( 'Field definition must be an HASH - skipping field' );
+                    next FIELD;
+                }
+  
+                my $type = $field->{type};
+  
+                if ( !$valid_types{$type} ) {
+                    $app->log->warn( "Invalid field type $type - falling back to 'text'" );
+                    $type = 'text';
+                }
+
+                local %request = %{ $c->tx->req->params->to_hash };
+  
+                my $sub        = $self->can( '_' . $type );
+                my $field_data = $self->$sub( $c, $field );
+                return $field_data->{field};
+            }
+        }
+    );
+}
+
+sub _text {
+    my ($self, $c, $field) = @_;
+
+    my $name  = $field->{name} // $field->{label} // '';
+    my $value = $request{$name} // $field->{data} // '';
+    my $id    = $field->{id} // $name;
+
+    return +{
+        label => $field->{label} // '',
+        field => $c->text_field( $name, $value, id => $id ),
+    };
 }
 
 1;
+
 __END__
 
 =encoding utf8
-
-=head1 NAME
-
-Mojolicious::Plugin::FormFieldsFromJSON - Mojolicious Plugin
 
 =head1 SYNOPSIS
 
@@ -28,16 +109,46 @@ Mojolicious::Plugin::FormFieldsFromJSON - Mojolicious Plugin
 
 L<Mojolicious::Plugin::FormFieldsFromJSON> is a L<Mojolicious> plugin.
 
-=head1 METHODS
+=head1 HELPER
 
-L<Mojolicious::Plugin::FormFieldsFromJSON> inherits all methods from
-L<Mojolicious::Plugin> and implements the following new ones.
+=head2 form_fields
 
-=head2 register
+  $controller->form_fields( 'formname' );
 
-  $plugin->register(Mojolicious->new);
+=head1 FIELD DEFINITIONS
 
-Register plugin in L<Mojolicious> application.
+This plugin supports several form fields:
+
+=over 4
+
+=item * text
+
+=item * checkbox
+
+=item * radio
+
+=item * select
+
+=back
+
+Those fields have the following definition items in common:
+
+=over 4
+
+=item * label
+
+=item * type
+
+=item * data
+
+=item * data_function
+
+=item * attributes
+
+Attributes of the field like "class"
+
+=back
+
 
 =head1 SEE ALSO
 
