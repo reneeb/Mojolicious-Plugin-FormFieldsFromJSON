7d7e868b46be8945f3202719857be5ef3bc15d2d
reneeb <github@renee-baecker.de>
2015-01-06 09:44:25 +0100

    when an arrayref is passed as first parameter for form_fields(), it is handled as field config.
    When you pass an arrayref as first parameter to form_fields(), it doesn't load the config from a file, but handles the
    arrayref as configuration for fields. This way, you can create your forms dynamically. This can be needed if your form depends on
    some user input.

--- lib/Mojolicious/Plugin/FormFieldsFromJSON.pm
+++ lib/Mojolicious/Plugin/FormFieldsFromJSON.pm
@@ -177,7 +177,7 @@ sub register {
   
             return '' if !$file;
   
-            if ( !$configs{$file} ) {
+            if ( !$configs{$file} && !ref $file ) {
                 my $path = File::Spec->catfile( $dir, $file . '.json' );
                 return '' if !-r $path;
   
@@ -196,9 +196,9 @@ sub register {
             }
  
             return if $params{only_load}; 
-            return '' if !$configs{$file};
+            return '' if !$configs{$file} && !ref $file;
   
-            my $field_config = $configs{$file};
+            my $field_config = $configs{$file} || $file;
 
             my @fields;
   
