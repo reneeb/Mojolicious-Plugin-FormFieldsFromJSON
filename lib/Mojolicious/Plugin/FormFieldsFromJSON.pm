f6dbe9b315c14c58ba0542cb65f11cefa490988f
reneeb <info@perl-services.de>
2021-04-08 17:46:26 +0200

    prepare release

--- lib/Mojolicious/Plugin/FormFieldsFromJSON.pm
+++ lib/Mojolicious/Plugin/FormFieldsFromJSON.pm
@@ -3,7 +3,7 @@ use Mojo::Base 'Mojolicious::Plugin';
 
 # ABSTRACT: create form fields based on a definition in a JSON file
 
-our $VERSION = '1.02';
+our $VERSION = '1.03';
 
 use Carp;
 use File::Basename;
